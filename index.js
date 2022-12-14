28efd7ee3495004523d8817b3e695b7449193e35
paanastasiadis <panag1234@outlook.com>
2022-12-14 17:56:31 +0200

    Get data, trilaterate and create the heatmap

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-import trilateration from './trilaterate.js';
+import trilateration from './trilateration.js';
+import measurement from './measurement.js';
+import map from './map.js';
+import arrayUtils from './array_utils.js';
 
-const p1 = trilateration.convertGeoToCartesian(37.418436, -121.963477);
-p1.r = 0.265710701754;
+// const p1 = measurement.convertGeodeticToEcef(37.418436, -121.963477);
+// p1.r = 265.710701754;
 
-const p2 = trilateration.convertGeoToCartesian(37.417243, -121.961889);
-p2.r = 0.234592423446;
+// const p2 = measurement.convertGeodeticToEcef(37.417243, -121.961889);
+// p2.r = 234.592423446;
 
-const p3 = trilateration.convertGeoToCartesian(37.418692, -121.960194);
-p3.r = 0.0548954278262;
+// const p3 = measurement.convertGeodeticToEcef(37.418692, -121.960194);
+// p3.r = 548.954278262;
 
-const trilPoint = trilateration.trilaterate(p1, p2, p3);
+// const trilPoint = trilateration.calculate(p1, p2, p3);
 
-const trilPointinGeo = trilateration.convertCartesianToGeo(trilPoint.x, trilPoint.y, trilPoint.z)
+// const trilPointinGeo = measurement.convertEcefToGeodetic(
+//   trilPoint.x,
+//   trilPoint.y,
+//   trilPoint.z
+// );
 
-console.log(p1, p2, p3, trilPointinGeo);
+// console.log(p1, p2, p3, trilPointinGeo);
 
 async function getRPIdevices() {
   try {
@@ -24,31 +31,153 @@ async function getRPIdevices() {
     console.error(err.message);
   }
 }
-getRPIdevices().then((data) => {
-  console.log(data);
-});
 
-const ws = new WebSocket('ws://62.217.127.19:8080/stream/');
-ws.addEventListener('open', (event) => {
-  console.log('Connection opened');
-});
+// setInterval(() => {
+//   const initData = {
+//     max: 100,
+//     min: 0,
+//     data: [],
+//   };
+//   map.heatmapLayer.setData(initData);
+// }, 2000);
 
-let devices = [];
-ws.addEventListener('message', (event) => {
-  devices = JSON.parse(event.data);
-  for (const device of devices) {
-    for (const measurement of device.measurements) {
-      measurement.distance = trilateration.convertRSSItoDistance(
-        measurement.signal_strength
-      );
-    }
-    // console.log(device);
+const initData = {
+  max: 100,
+  min: 0,
+  data: [],
+};
+map.heatmapLayer.setData(initData);
+
+getRPIdevices().then((rpiDevices) => {
+  for (const device of rpiDevices) {
+    device.id = parseInt(device.Name.replace(/^\D+/g, ''));
+    map.addPin(device.Latitude, device.Longitude, device.Name);
   }
-  // console.log(`Server sent the following message: ${event.data}`);
-});
-ws.addEventListener('close', (event) => {
-  console.log('Connection closed.');
-});
-ws.addEventListener('error', (event) => {
-  console.log(`WebSocket error: ${event}.`);
+
+  // console.log(rpiDevices);
+
+  const ws = new WebSocket('ws://62.217.127.19:8080/stream/');
+  ws.addEventListener('open', (event) => {
+    console.log('Connection opened');
+  });
+
+  // let isFetchingInitData = true;
+  let devices = [];
+
+  // setInterval(() => {
+  //   console.log('Updating people...');
+
+  //   const updatedDevices = devices.filter((device) => {
+  //     const currTime = new Date();
+  //     const deviceTime = new Date(device.timestamp);
+  //     const timeDiff = (currTime.getTime() - deviceTime.getTime()) / 60000;
+  //     console.log(
+  //       currTime.getTime(),
+  //       deviceTime.getTime(),
+  //       timeDiff.toFixed(2)
+  //     );
+  //     return timeDiff.toFixed(2) < 1;
+  //   });
+  //   const updatedData = {
+  //     max: 100,
+  //     min: 0,
+  //     data: updatedDevices,
+  //   };
+  //   map.heatmapLayer.setData(updatedData);
+  //   devices = [...updatedDevices];
+  // }, 120100);
+
+  ws.addEventListener('message', (event) => {
+    let oldLength = devices.length;
+    const foundDevices = JSON.parse(event.data);
+    const updatedDevices = devices.filter((device) => {
+      const currTime = new Date();
+      const deviceTime = new Date(device.timestamp);
+      const timeDiff = (currTime.getTime() - deviceTime.getTime()) / 60000;
+      // console.log(
+      //   currTime.getTime(),
+      //   deviceTime.getTime(),
+      //   timeDiff.toFixed(2)
+      // );
+      return timeDiff.toFixed(2) < 2;
+    });
+    const updatedData = {
+      max: 100,
+      min: 0,
+      data: updatedDevices,
+    };
+    map.heatmapLayer.setData(updatedData);
+    devices = [...updatedDevices];
+    console.log(
+      `${oldLength - devices.length} devices were removed! New length: ${
+        devices.length
+      }, Old length: ${oldLength}`
+    );
+    let i = 0;
+    oldLength = devices.length;
+    for (const foundDevice of foundDevices) {
+      const rpiCombinations = arrayUtils.getArrayElementsCombs(
+        foundDevice.measurements,
+        3
+      );
+      for (const combination of rpiCombinations) {
+        const rpi1 = rpiDevices.find((rpi) => rpi.id === combination[0].rpi);
+        const rpi2 = rpiDevices.find((rpi) => rpi.id === combination[1].rpi);
+        const rpi3 = rpiDevices.find((rpi) => rpi.id === combination[2].rpi);
+
+        const sphere1 = measurement.getCartesianSphere(
+          rpi1.Latitude,
+          rpi1.Longitude,
+          foundDevice.measurements[0].signal_strength
+        );
+        const sphere2 = measurement.getCartesianSphere(
+          rpi2.Latitude,
+          rpi2.Longitude,
+          foundDevice.measurements[1].signal_strength
+        );
+        const sphere3 = measurement.getCartesianSphere(
+          rpi3.Latitude,
+          rpi3.Longitude,
+          foundDevice.measurements[2].signal_strength
+        );
+
+        const trilPoint = trilateration.calculate(sphere1, sphere2, sphere3);
+        // console.log(trilPoint);
+        if (trilPoint) {
+          const trilCoords = measurement.convertEcefToGeodetic(
+            trilPoint.x,
+            trilPoint.y,
+            trilPoint.z
+          );
+
+          map.heatmapLayer.addData({
+            lat: trilCoords.latitude,
+            lng: trilCoords.longitude,
+            count: 30,
+          });
+
+          //adding all devices
+          devices.push({
+            lat: trilCoords.latitude,
+            lng: trilCoords.longitude,
+            count: 30,
+            timestamp: Date.now(),
+          });
+          i++;
+          break;
+        }
+      }
+    }
+    console.log('New Devices: ' + foundDevices.length);
+    console.log('Located Devices: ' + i);
+    console.log(`Buffer Length: ${devices.length}, old length: ${oldLength}`);
+
+    // console.log(`Server sent the following message: ${event.data}`);
+  });
+  ws.addEventListener('close', (event) => {
+    console.log('Connection closed.');
+  });
+  ws.addEventListener('error', (event) => {
+    console.log(`WebSocket error: ${event}.`);
+  });
 });
